49a6b7a29c89a53b31b87623a270701f8daad3e5
Mohammad Hosein Lashani <sapumr@gmail.com>
2025-04-16 17:54:52 +0330

    Update heading styles for improved spacing in mdx-components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -8,37 +8,37 @@ import { codeToHtml } from "shiki";
 const components = {
   h1: (props: ComponentPropsWithoutRef<"h1">) => (
     <h1
-      className="mt-6 mb-5 scroll-m-20 font-mono text-4xl font-extrabold tracking-tight"
+      className="mt-4 mb-6 scroll-m-20 font-mono text-4xl font-extrabold tracking-tight"
       {...props}
     />
   ),
   h2: (props: ComponentPropsWithoutRef<"h2">) => (
     <h2
-      className="mt-6 mb-5 scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0"
+      className="mt-4 mb-6 scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0"
       {...props}
     />
   ),
   h3: (props: ComponentPropsWithoutRef<"h3">) => (
     <h3
-      className="mt-6 mb-5 scroll-m-20 text-2xl font-semibold tracking-tight"
+      className="mt-4 mb-6 scroll-m-20 text-2xl font-semibold tracking-tight"
       {...props}
     />
   ),
   h4: (props: ComponentPropsWithoutRef<"h4">) => (
     <h4
-      className="mt-6 mb-5 scroll-m-20 text-xl font-semibold tracking-tight"
+      className="mt-4 mb-6 scroll-m-20 text-xl font-semibold tracking-tight"
       {...props}
     />
   ),
   h5: (props: ComponentPropsWithoutRef<"h5">) => (
     <h5
-      className="mt-6 mb-5 scroll-m-20 text-lg font-semibold tracking-tight"
+      className="mt-4 mb-6 scroll-m-20 text-lg font-semibold tracking-tight"
       {...props}
     />
   ),
   h6: (props: ComponentPropsWithoutRef<"h6">) => (
     <h6
-      className="mt-6 mb-5 scroll-m-20 text-base font-semibold tracking-tight"
+      className="mt-4 mb-6 scroll-m-20 text-base font-semibold tracking-tight"
       {...props}
     />
   ),
