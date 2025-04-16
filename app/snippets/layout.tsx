379a6f37302f712e36a025a0c79bc1ca57df4dcd
Mohammad Hosein Lashani <sapumr@gmail.com>
2025-04-16 23:46:52 +0330

    Update layout and mdx-components styles; adjust max-width properties for better responsiveness and layout consistency.

--- app/snippets/layout.tsx
+++ app/snippets/layout.tsx
@@ -2,7 +2,7 @@ import Aside from "@/app/snippets/_components/Aside";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto grid min-h-svh w-full max-w-7xl gap-5 p-4 md:grid-cols-[14rem_1fr]">
+    <div className="mx-auto grid min-h-svh w-full max-w-7xl gap-5 px-4 md:grid-cols-[14rem_1fr]">
       <Aside />
 
       <article>{children}</article>
