dcf98219f6b9410deaf7dbe89c950e4848d66dac
Mohammad Hosein Lashani <sapumr@gmail.com>
2025-04-22 13:34:53 +0330

    Add Radix UI components: Label and Select; integrate Sonner for notifications

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { ReactNode } from "react";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
@@ -10,6 +11,7 @@ export default function Providers({ children }: { children: ReactNode }) {
       disableTransitionOnChange
     >
       {children}
+      <Toaster richColors closeButton />
     </ThemeProvider>
   );
 }
