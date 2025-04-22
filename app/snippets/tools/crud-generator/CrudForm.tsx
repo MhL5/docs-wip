335d6ddabd1b19fa0954e228d00d6f7bb8b067a6
Mohammad Hosein Lashani <sapumr@gmail.com>
2025-04-22 20:10:31 +0330

    Enhance CRUD generator by adding support for query hooks and mutation success messages; refactor function generation logic for improved clarity and functionality.

--- app/snippets/tools/crud-generator/CrudForm.tsx
+++ app/snippets/tools/crud-generator/CrudForm.tsx
@@ -18,6 +18,7 @@ import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 
 const methods = ["post", "get", "delete", "put", "patch"] as const;
+const queryHooks = ["useSuspenseQuery", "useQuery"] as const;
 const apiDefaultErrorMessage =
   "مشکلی در هنگام دریافت X رخ داده است! لطفا دوباره تلاش کنید.";
 type Options = {
@@ -26,17 +27,19 @@ type Options = {
   defaultErrorMessage: string;
   route: string;
   version: "legacy" | "latest";
+  queryHook: "useSuspenseQuery" | "useQuery";
+  mutationSuccessMessage: string;
 };
 
-function generateResult({
-  version,
-  name,
-  route,
+function generateFunctionName({
   method,
-  defaultErrorMessage,
-}: Options) {
-  if (!name) return ``; // Simplified check
-
+  name,
+  capitalFirstWord = false,
+}: {
+  method: Options["method"];
+  name: Options["name"];
+  capitalFirstWord?: boolean;
+}) {
   const renameMethod = {
     post: "create",
     get: "get",
@@ -44,8 +47,21 @@ function generateResult({
     put: "update",
     patch: "update",
   };
+  const firstWord = capitalFirstWord
+    ? `${renameMethod?.[method]?.[0]?.toUpperCase() + renameMethod?.[method]?.slice(1)}`
+    : `${renameMethod?.[method]}`;
 
-  const functionName = `${renameMethod[method]}${name[0].toUpperCase() + name.slice(1)}`;
+  return `${firstWord}${name?.[0]?.toUpperCase() + name?.slice(1)}`;
+}
+
+function generateAsyncFunction({
+  version,
+  name,
+  route,
+  method,
+  defaultErrorMessage,
+}: Options) {
+  if (!name) return ``; // Simplified check
 
   // Version-specific details
   const {
@@ -74,11 +90,18 @@ function generateResult({
           catchFunction: "catchKyErrorTyped",
         };
 
-  // Common template parts
+  const paramsTypeName = generateFunctionName({
+    method,
+    name,
+    capitalFirstWord: true,
+  });
+  const isGetMethod = method === "get";
+
   const commonImports = `
-// todo: add types
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type ApiResponse = any;`;
+type ApiResponse = any;
+
+${isGetMethod ? "" : `export type ${paramsTypeName}Params = any;`}
+`;
 
   const commonErrorHandling = `
 const defaultErrorMessage = \`${defaultErrorMessage}\`;
@@ -93,9 +116,9 @@ function getErrorMessage(errorMessage: string | undefined) {
 }`;
 
   const commonFunctionBody = `
-export async function ${functionName}(): ${resultType}<ApiResponse> {
+export async function ${generateFunctionName({ method, name })}(${isGetMethod ? "" : `params:${paramsTypeName}`}): ${resultType}<ApiResponse> {
   const [error, data] = await ${catchFunction}<ApiResponse>(
-    ${apiObject}.${method}('${route}').json(),
+    ${apiObject}.${method}('${route}'${isGetMethod ? "" : `{json: params}`}).json(),
   );
 
   if (error || !data)
@@ -118,18 +141,100 @@ ${commonFunctionBody}
 `;
 }
 
-export default function CrudForm({
-  result,
-}: {
-  result: (data: string) => ReactNode;
-}) {
+function generateTanStackQueryHooks({
+  name,
+  method,
+  queryHook,
+  mutationSuccessMessage,
+}: Options): string[] {
+  const functionName = generateFunctionName({
+    method,
+    name,
+  });
+  const hookName = generateFunctionName({
+    method,
+    name,
+    capitalFirstWord: true,
+  });
+
+  if (method === "get") {
+    const queryOptions = `import { ${functionName} } from "@/features/${name}/services/${functionName}";
+import { queryOptions } from "@tanstack/react-query";
+
+export const ${functionName}QueryOptions = () => {
+  return queryOptions({
+    queryKey: [\`${functionName}\`],
+    queryFn: async () => {
+      const response = await ${functionName}();
+      if (!response.success) return response.message;
+      return response.data;
+    },
+  });
+};
+`;
+    const query = `"use client";
+
+import { ${functionName}QueryOptions } from "@/features/${name}/hooks/${functionName}QueryOptions";
+import { ${queryHook} } from "@tanstack/react-query";
+
+export function use${hookName}() {
+  return ${queryHook}(${functionName}QueryOptions());
+}`;
+
+    return [queryOptions, query];
+  }
+
+  const paramsTypeName = generateFunctionName({
+    method,
+    name,
+    capitalFirstWord: true,
+  });
+  const mutation = `"use client";
+
+import {
+  ${functionName},
+  type ${paramsTypeName}Params,
+} from "@/features/${name}/services/${functionName}";
+import { ${functionName}QueryOptions } from "@/features/${name}/hooks/${functionName}QueryOptions";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+export default function use${hookName}() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (params: ${paramsTypeName}Params) => {
+      const response = await ${functionName}(params);
+      if (!response.success) throw new Error(response.message);
+      return response.data;
+    },
+    onSuccess: () => {
+      toast.success('${mutationSuccessMessage}');
+      queryClient.invalidateQueries({
+        queryKey: [${functionName}QueryOptions().queryKey],
+      });
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+}
+`;
+  return [mutation];
+}
+
+export default function CrudForm() {
   const [states, setStates] = useState<Options>({
     version: "latest",
     name: "",
     method: "get",
+    queryHook: "useSuspenseQuery",
     defaultErrorMessage: apiDefaultErrorMessage,
     route: "",
+    mutationSuccessMessage: "",
   });
+
+  const isGetMethod = states.method === "get";
 
   return (
     <fieldset className="my-8 flex flex-col gap-5 rounded-md border p-5">
@@ -204,7 +309,7 @@ export default function CrudForm({
           <div className="flex flex-col gap-2">
             <Label className="">Method:</Label>
             <SelectTrigger className="w-full">
-              <SelectValue placeholder="Select a verified email to display" />
+              <SelectValue placeholder="" />
             </SelectTrigger>
           </div>
           <SelectContent>
@@ -217,19 +322,89 @@ export default function CrudForm({
             })}
           </SelectContent>
         </Select>
+
+        {!isGetMethod ? (
+          <div className="flex flex-col gap-2">
+            <Label>Mutation Success Message:</Label>
+            <Input
+              type="text"
+              dir="auto"
+              value={states.mutationSuccessMessage}
+              onChange={(e) =>
+                setStates((s) => ({
+                  ...s,
+                  mutationSuccessMessage: e.target.value,
+                }))
+              }
+            />
+          </div>
+        ) : (
+          <Select
+            onValueChange={(value) => {
+              const isValidQueryHook = queryHooks.includes(
+                value as (typeof queryHooks)[number],
+              );
+
+              if (!isValidQueryHook) return;
+
+              setStates((s) => ({
+                ...s,
+                queryHook: value as (typeof queryHooks)[number],
+              }));
+            }}
+            defaultValue={states.queryHook}
+          >
+            <div className="flex flex-col gap-2">
+              <Label className="">Query Hook type:</Label>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="" />
+              </SelectTrigger>
+            </div>
+            <SelectContent>
+              {queryHooks.map((method) => {
+                return (
+                  <SelectItem key={method} value={method}>
+                    {method}
+                  </SelectItem>
+                );
+              })}
+            </SelectContent>
+          </Select>
+        )}
       </form>
 
       <Suspense>
+        {generateTanStackQueryHooks(states)?.map((tqh, i) => {
+          return (
+            <div
+              key={`generateTanStackQueryHooks-${tqh.slice(0, 10)}-${i}`}
+              className={`${tqh ? "h-fit" : "h-0"} bg-code-background relative overflow-y-hidden rounded-lg transition-all duration-500 ease-linear`}
+            >
+              {tqh && (
+                <div className="p-4">
+                  <CopyButton
+                    content={tqh}
+                    className="absolute top-3 right-3"
+                  />
+                  <Code lang="tsx">{tqh}</Code>
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </Suspense>
+
+      <Suspense>
         <div
-          className={`${generateResult(states) ? "h-160" : "h-0"} bg-code-background relative overflow-y-hidden rounded-lg transition-all duration-500 ease-linear`}
+          className={`${generateAsyncFunction(states) ? "h-fit" : "h-0"} bg-code-background relative overflow-y-hidden rounded-lg transition-all duration-500 ease-linear`}
         >
-          {generateResult(states) && (
+          {generateAsyncFunction(states) && (
             <div className="p-4">
               <CopyButton
-                content={generateResult(states)}
+                content={generateAsyncFunction(states)}
                 className="absolute top-3 right-3"
               />
-              <Code lang="tsx">{generateResult(states)}</Code>
+              <Code lang="tsx">{generateAsyncFunction(states)}</Code>
             </div>
           )}
         </div>
